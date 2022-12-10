08ba4d63bb336c905a2d6e65094a20e9813a96b9
Theodore Stavrianos <theodore@theo-s.eu>
2022-12-10 18:33:14 +0000

    first commit

---
+++ FO4SwitchLightShadows/Settings.cs
@@ -0,0 +1,15 @@
+﻿using Mutagen.Bethesda.WPF.Reflection.Attributes;
+
+namespace FO4SwitchLightShadows
+{
+    public enum RunMode
+    {
+        RemoveShadows,
+        AddShadows
+    }
+    public class Settings
+    {
+        [MaintainOrder] [SettingName("Run Mode")]
+        public RunMode Mode { get; set; } = RunMode.RemoveShadows;
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Port of zEdit - Lights dont cast shadows by Myrdo to Synthesis.
+
+In addition, I have added the option to work in reverse (switch any NonShadowSpotlight to ShadowSpotlight).
